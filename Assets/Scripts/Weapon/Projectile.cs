910d5a560015000de0e29d7d72a1626ca52966bd
JoeyMBrown <joey.ma.brown@gmail.com>
2025-03-25 21:17:14 -0400

    Add new projectile game objects along with basic projectile class

---
+++ Assets/Scripts/Weapon/Projectile.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private float speed;
+
+    public Vector3 Direction { get; set; }
+
+    // Move the projectile every frame in the given direction
+    // at the given speed.
+    private void Update()
+    {
+        transform.Translate(Direction * (speed * Time.deltaTime));
+    }
+
+    // An event that is called when collider enters 2D trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision);
+    }
+}
