ebcc71b3cc080ce1e61e3d439672a7b8afdf2c5a
JoeyMBrown <joey.ma.brown@gmail.com>
2025-03-25 21:30:23 -0400

    Create weapon scriptable object, add 4 magic and 4 melee weapons as SO

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public enum WeaponType
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    Magic,
+    Melee
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+// This places this within the "right click" list of the unity editor
+// We can now right click inside of unity and select to create a new
+// Weapon scriptable object.
+[CreateAssetMenu(fileName = "Weapon_")]
+public class Weapon : ScriptableObject
+{
+    [Header("Config")]
+    public Sprite Icon;
+    public WeaponType WeaponType;
+    public float Damage;
+
+    [Header("Projectile")]
+    // The projectile the weapon uses.
+    public Projectile ProjectilePrefab;
+    public float RequiredMana;
 }
