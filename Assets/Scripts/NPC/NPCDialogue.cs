5228737a899628f0a03b0710f260e17f5c7fc4e5
JoeyMBrown <joey.ma.brown@gmail.com>
2025-04-08 20:17:10 -0400

    Add NPC Dialogue UI

---
+++ Assets/Scripts/NPC/NPCDialogue.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public enum InterationType
+{
+    Quest,
+    Shop
+}
+
+[CreateAssetMenu(menuName = "NPC Dialogue")]
+public class NPCDialogue : ScriptableObject
+{
+    [Header("Info")]
+    public string Name;
+    public Sprite Icon; // Icon of NPC.
+
+    [Header("Interaction")]
+    public bool HasInteraction;
+    public InterationType InterationType;
+
+    [Header("Dialogue")]
+    [TextArea]public string[] Dialogue;
+}
