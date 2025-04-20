9146a3a34409988501b2e2739a9365a954d3715b
JoeyMBrown <joey.ma.brown@gmail.com>
2025-04-20 13:13:18 -0400

    Add logic to build crafting recipes, add crafting manager

---
+++ Assets/Scripts/Crafting/CraftingManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CraftingManager : Singleton<CraftingManager>
+{
+    [Header("Config")]
+    [SerializeField] private RecipeCard recipeCardPrefab;
+    [SerializeField] private Transform recipeContainer;
+
+    [Header("Recipes")]
+    [SerializeField] private RecipeList recipes;
+
+    private void Start()
+    {
+        LoadRecipes();
+    }
+
+    private void LoadRecipes()
+    {
+        for (int i = 0; i < recipes.Recipes.Length; i ++)
+        {
+            RecipeCard card = Instantiate(recipeCardPrefab, recipeContainer);
+            card.InitRecipeCard(recipes.Recipes[i]);
+        }
+    }
+}
